9a4ef1ca1deace77aee6984da390251a2a74a360
Abilio Krismanuel <78332696+abiliokrismanuel@users.noreply.github.com>
2022-06-24 20:47:05 +0700

    final

---
+++ final_71190498/app/src/main/java/id/ac/ukdw/final_71190498/fragments/ProfileFragment.kt
@@ -0,0 +1,139 @@
+package id.ac.ukdw.final_71190498.fragments
+
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import id.ac.ukdw.final_71190498.MainActivity
+import id.ac.ukdw.final_71190498.R
+import id.ac.ukdw.final_71190498.databinding.ActivityProfileBinding
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.net.toUri
+import androidx.viewbinding.ViewBinding
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import id.ac.ukdw.final_71190498.Movie
+import id.ac.ukdw.final_71190498.ProfileActivity
+import java.net.URI
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ProfileFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ProfileFragment : Fragment() {
+    //view binding
+    private lateinit var binding: ActivityProfileBinding
+
+    //firebase auth
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private var infoList:ArrayList<String> = ArrayList()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        // Inflate the layout for this fragment
+//            binding = ActivityProfileBinding.inflate(layoutInflater, container, false)
+
+
+        //init firebase auth
+            firebaseAuth = FirebaseAuth.getInstance()
+            checkUser()
+
+
+            //handle click, logout user
+            val i = inflater.inflate(R.layout.fragment_profile, container, false)
+            val logoutBtn = i.findViewById<Button>(R.id.logoutBtn)
+            val emailTv = i.findViewById<TextView>(R.id.emailTv)
+            val uphoto = i.findViewById<ImageView>(R.id.ivProfile)
+            val dname = i.findViewById<TextView>(R.id.nameTv)
+            checkUser()
+
+            emailTv.setText(infoList.get(0))
+            dname.setText(infoList.get(1))
+            val imgUrl: String = infoList.get(2)
+
+            Glide.with(this).load(imgUrl).apply(RequestOptions.circleCropTransform()).into(uphoto)
+
+            logoutBtn.setOnClickListener{
+                firebaseAuth.signOut()
+                checkUser()
+        }
+        return i
+    }
+
+    private fun checkUser() {
+        //get current user
+        val firebaseUser = firebaseAuth.currentUser
+        infoList.clear()
+        if (firebaseUser==null){
+            //user not logged in
+            startActivity(Intent(activity, MainActivity::class.java))
+            activity?.finish()
+        }else{
+            val email = firebaseUser.email
+            val dname = firebaseUser.displayName
+            val uphoto = firebaseUser.photoUrl
+            infoList.add(email.toString())
+            infoList.add(dname.toString())
+            infoList.add(uphoto.toString())
+        }
+    }
+
+//    private fun checkUserLogin() {
+//        //get current user
+//        val firebaseUser = firebaseAuth.currentUser
+//        infoList.clear()
+//
+//        if (firebaseUser!=null){
+//            //user not logged in
+//            val email = firebaseUser.email
+//            val dname = firebaseUser.displayName
+//            val uphoto = firebaseUser.photoUrl
+//            infoList.add(email.toString())
+//            infoList.add(dname.toString())
+//            infoList.add(uphoto.toString())
+//        }
+//    }
+
+
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment ProfileFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            ProfileFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
+}
---
+++ final_71190498/app/src/main/java/id/ac/ukdw/final_71190498/fragments/SearchFragment.kt
@@ -0,0 +1,187 @@
+package id.ac.ukdw.final_71190498.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.database.*
+import id.ac.ukdw.final_71190498.CrudActivity
+import id.ac.ukdw.final_71190498.Movie
+import id.ac.ukdw.final_71190498.MovieAdapter
+import id.ac.ukdw.final_71190498.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SearchFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SearchFragment : Fragment() {
+    private lateinit var dbref: DatabaseReference
+    private lateinit var movieRecyclerView: RecyclerView
+    private lateinit var movieArrayList : ArrayList<Movie>
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val i = inflater.inflate(R.layout.fragment_search, container, false)
+        val searchBtn = i.findViewById<ImageButton>(R.id.searchBtn)
+        val etSearch = i.findViewById<TextInputEditText>(R.id.etSearch)
+
+        movieRecyclerView = i.findViewById(R.id.rv_movie)
+        movieRecyclerView.setHasFixedSize(true)
+        movieArrayList = arrayListOf<Movie>()
+
+
+        movieRecyclerView.adapter = MovieAdapter(movieArrayList)
+//        getMovieData()
+
+
+        searchBtn.setOnClickListener {
+            val cari = etSearch.text.toString()
+
+            if(cari.isNotEmpty()){
+                readData(cari.trim())
+            }else{
+                Toast.makeText(activity,"Masukkan Kata Kunci Pencarian", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        // Inflate the layout for this fragment
+        return i
+    }
+
+
+    private fun getMovieData() {
+
+        dbref = FirebaseDatabase.getInstance().getReference("Movie")
+        dbref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var movieList:ArrayList<Movie> = ArrayList()
+                movieList.clear()
+
+                if(snapshot.exists()){
+                    for (movieSnapshot in snapshot.children){
+                        val movie = movieSnapshot.getValue(Movie::class.java)
+                        movieList.add(movie!!)
+                    }
+                    var movieAdapter = MovieAdapter(movieList)
+                    movieRecyclerView.apply {
+                        movieRecyclerView.layoutManager = LinearLayoutManager(context)
+                        movieRecyclerView.adapter = movieAdapter
+                    }
+
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+    }
+
+    private fun readData(cari: String){
+        dbref = FirebaseDatabase.getInstance().getReference("Movie")
+        dbref.orderByChild("judul").equalTo(cari).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var movieList:ArrayList<Movie> = ArrayList()
+                movieList.clear()
+
+                if(snapshot.exists()){
+                    for (movieSnapshot in snapshot.children){
+                        val movie = movieSnapshot.getValue(Movie::class.java)
+                        movieList.add(movie!!)
+                    }
+                    var movieAdapter = MovieAdapter(movieList)
+                    movieRecyclerView.apply {
+                        movieRecyclerView.layoutManager = LinearLayoutManager(context)
+                        movieRecyclerView.adapter = movieAdapter
+                    }
+
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        dbref.orderByChild("tahun").equalTo(cari).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var movieList:ArrayList<Movie> = ArrayList()
+                movieList.clear()
+
+                if(snapshot.exists()){
+                    for (movieSnapshot in snapshot.children){
+                        val movie = movieSnapshot.getValue(Movie::class.java)
+                        movieList.add(movie!!)
+                    }
+                    var movieAdapter = MovieAdapter(movieList)
+                    movieRecyclerView.apply {
+                        movieRecyclerView.layoutManager = LinearLayoutManager(context)
+                        movieRecyclerView.adapter = movieAdapter
+                    }
+
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        dbref.orderByChild("genre").equalTo(cari).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var movieList:ArrayList<Movie> = ArrayList()
+                movieList.clear()
+
+                if(snapshot.exists()){
+                    for (movieSnapshot in snapshot.children){
+                        val movie = movieSnapshot.getValue(Movie::class.java)
+                        movieList.add(movie!!)
+                    }
+                    var movieAdapter = MovieAdapter(movieList)
+                    movieRecyclerView.apply {
+                        movieRecyclerView.layoutManager = LinearLayoutManager(context)
+                        movieRecyclerView.adapter = movieAdapter
+                    }
+
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        }
+    }
+
+
+
+
